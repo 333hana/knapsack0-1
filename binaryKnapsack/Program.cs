234505360abbc64b6dde8ac3fdce77831ed0d18e
Hana <hananassef@yahoo.com>
2020-08-22 19:55:08 +0200

    initial commit main Knapsack variables

---
+++ binaryKnapsack/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace binaryKnapsack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] val = new int[] { 60, 120, 100 };
+            int[] wt = new int[] { 10, 30, 20 };
+            int[] x = new int[] { 0, 0, 0 };
+            int[] y = new int[] { 0, 0, 0 };
+            int W = 50;
+            int n = val.Length;
+            Console.WriteLine("knapsack capacity is: " + W);
+            Console.WriteLine("knapsack contains " + n + " items.");
+            for (int i = 0; i < wt.Length; i++)
+            {
+                Console.WriteLine("Item '" + i + "' weight is " + wt[i]);
+                Console.WriteLine("Item '" + i + "' value is " + val[i]);
+                Console.WriteLine("...............................");
+            }
+        }
+    }
+}
